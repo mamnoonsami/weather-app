3831a3a4496c65ca5253b5e0a0b1d72517eac2b7
mamnoonsami <mamnoon909@gmail.com>
2021-08-05 22:57:12 -0300

    California's weather task

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
-console.log("STARTING")
+const request = require('request')
 
-setTimeout(() => {
-    console.log('2 seconds timer')
-}, 2000)
+// the url : http://api.weatherstack.com/current?access_key=c21bb0678df7916a8d46702a81125349&query=37.8267,-122.4233
+// if the weather has to be in farenheight http://api.weatherstack.com/current?access_key=c21bb0678df7916a8d46702a81125349&query=37.8267,-122.4233&units=f
 
-setTimeout(() => {
-    console.log('0 second timer')
+const url = 'http://api.weatherstack.com/current?access_key=c21bb0678df7916a8d46702a81125349&query=37.8267,-122.4233'
 
-}, 0);
-
-console.log("STOPPING")
-
-console.log("new line");
+request({ url: url, json: true }, (error, response) => { // "json: true" means the response will be automatically parsed as JSON, so that we do not need to parse by writing Json.parse() function
+    //console.log(response.body.location) //doesnot work,because I am not premium member
+    console.log(response.body.current.weather_descriptions[0])
+    console.log("It is " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees")
+})
