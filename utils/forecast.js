5a59d8068114d17643691d899a70c3a6bd6ad023
mamnoonsami <mamnoon909@gmail.com>
2021-08-07 15:12:14 -0300

    geocode and forecast api is separated in 2 files (previous one was error

---
+++ utils/forecast.js
@@ -0,0 +1,27 @@
+const request = require('request')
+
+/* the below function returns the weather forecast for 
+the given longitude and latitude 
+the url : http://api.weatherstack.com/current?access_key=c21bb0678df7916a8d46702a81125349&query=37.8267,-122.4233
+if the weather has to be in farenheight http://api.weatherstack.com/current?access_key=c21bb0678df7916a8d46702a81125349&query=37.8267,-122.4233&units=f
+*/
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=c21bb0678df7916a8d46702a81125349&query=' + latitude + ',' + longitude
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) {
+            callback("Sorry! Unable to connect to weather service", undefined)
+        } else if (response.body.error) {
+            callback("Unable to find the location. Try again.", undefined)
+        } else {
+            callback(undefined, {
+                weather_description: response.body.current.weather_descriptions[0],
+                temperature: response.body.current.temperature,
+                feels_like: response.body.current.feelslike
+            })
+        }
+    })
+}
+
+module.exports = forecast
---
+++ utils/geocode.js
@@ -0,0 +1,25 @@
+const request = require('request')
+
+/*
+Geocoding
+mapbox api is used to get the longitude and latitude to get the address
+*/
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWFtbm9vbjcyNjQiLCJhIjoiY2tzMTB1cjVuMGlqbDJ2bzNkNnNsZm95bSJ9.rY0cfMHJkOPOAze6KDoriw&limit=1'
+
+    request({ url: url, json: true }, (error, response) => {
+        if (error) { //if the wifi is off or other low level errors occur
+            callback('Unable to connect to loaction services!', undefined)
+        } else if (response.body.features.length === 0) { //if the location is not valid or doesnot exist
+            callback('Unable to find the location. Try again.', undefined)
+        } else { //everything alright, sends the data
+            callback(undefined, {
+                latitude: response.body.features[0].center[0],
+                longitude: response.body.features[0].center[1],
+                location: response.body.features[0].place_name,
+            })
+        }
+    })
+}
+
+module.exports = geocode
